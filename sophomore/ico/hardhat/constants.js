468ad1326877330898c5d2076b1ca4d178731777
Gerry Saporito <gerrysaporito@gmail.com>
2022-07-05 18:44:32 -0400

    Created ICO app

---
+++ sophomore/ico/hardhat/constants.js
@@ -0,0 +1,5 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS =
+  '0x07CF6f04892EcFff8cC14bE1ed9F41b4d57FfC55';
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
